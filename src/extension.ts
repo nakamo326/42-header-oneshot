6bae6098b356ccd36c9f17a93f37430d25661479
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-17 11:26:34 +0900

    add getConfig func

--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,54 @@ import * as vscode from 'vscode';
 import { env } from 'process';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "42-header-oneshot" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand('42-header-oneshot.addHeader', () => {
-    const user = env.USER;
-    const mail = env.MAIL;
-    vscode.window.showInformationMessage(`USER is ${user} and MAIL is ${mail}`);
+    console.log('Congratulations, your extension "42-header-oneshot" is now active!');
+
+    const rowLen = 80;
+    const template = `/* ************************************************************************** */
+    /*                                                                            */
+    /*                                                        :::      ::::::::   */
+    /*   $file                                              :+:      :+:    :+:   */
+    /*                                                    +:+ +:+         +:+     */
+    /*   By: $user <$mail>                              +#+  +:+       +#+        */
+    /*                                                +#+#+#+#+#+   +#+           */
+    /*   Created: 2021/12/15 21:44:53 by $user             #+#    #+#             */
+    /*   Updated: 2021/12/15 21:44:57 by $user            ###   ########.fr       */
+    /*                                                                            */
+    /* ************************************************************************** */
+    
+    `;
+
+    const [user, mail] = getConfig();
+    if (user === undefined || mail === undefined) {
+      vscode.window.showErrorMessage(
+        `User or Mail doesn't exist in environment variables and configurations.
+        Please export "USER" or "MAIL" to your environment or add configurations.`,
+      );
+      return;
+    }
+    console.log(`USER is ${user} and MAIL is ${mail}`);
   });
 
   context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
+
+function getConfig(): [string | undefined, string | undefined] {
+  // if key isn't in env, ret is undefined.
+  const userEnv = env.USER;
+  const mailEnv = env.MAIL;
+
+  // if there are no input in config, return is empty string.
+  const userConfig: string | undefined = vscode.workspace
+    .getConfiguration()
+    .get('42-header-oneshot.user');
+  const mailConfig: string | undefined = vscode.workspace
+    .getConfiguration()
+    .get('42-header-oneshot.mail');
+
+  const user: string | undefined = userConfig !== '' ? userConfig : userEnv;
+  const mail: string | undefined = mailConfig !== '' ? mailConfig : mailEnv;
+  return [user, mail];
+}

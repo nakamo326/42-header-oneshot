1937609b221437bf87e43156f6089f81ec86776e
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-17 10:06:58 +0900

    change command name and get env

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,19 @@
 import * as vscode from 'vscode';
+import { env } from 'process';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "42-header-oneshot" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('42-header-oneshot.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from 42 Header OneShot!');
+  let disposable = vscode.commands.registerCommand('42-header-oneshot.addHeader', () => {
+    const user = env.USER;
+    const mail = env.MAIL;
+    vscode.window.showInformationMessage(`USER is ${user} and MAIL is ${mail}`);
   });
 
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
